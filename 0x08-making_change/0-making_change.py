2fa514fb69d83195e0e8ff4860681043dc078d50
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 21:45:37 +0000

    Last trial

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,10 +6,9 @@ def makeChange(coins, total):
     """Function to return minimum coin number for change"""
     if total <= 0:
         return 0
-    coins.sort()
+
     dp = [total + 1] * (total + 1)
     dp[0] = 0
-
 
     for j in coins:
         if j > total:
