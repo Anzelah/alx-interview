059043e61e2c5da16d5fd1096a69f9792f55ee03
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 21:07:58 +0000

    try sorting

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,8 +10,9 @@ def makeChange(coins, total):
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
-    coins.sort(reverse=True)
     for j in coins:
+        if j > total:
+            break
         for i in range(j, total + 1):
             dp[i] = min(dp[i], dp[i - j] + 1)
 
