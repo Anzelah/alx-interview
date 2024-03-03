0de75f4609542218868e38a196fb459027738a10
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 20:17:41 +0000

    remove unnecessary calculations

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,7 +7,7 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [total + 1] * (total + 1)
+    dp = [float('inf')] * (total + 1)
     dp[0] = 0
 
     for i in range(1, total + 1):
@@ -15,4 +15,4 @@ def makeChange(coins, total):
             if (i - j >= 0):
                 dp[i] = min(dp[i], 1 + dp[i - j])
 
-    return dp[total] if dp[total] != total + 1 else -1
+    return dp[total] if dp[total] != float('inf') else -1
