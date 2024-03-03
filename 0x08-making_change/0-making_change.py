0068022796cc4fa3e51ea5c4e6b9d5b59fd53812
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 20:51:10 +0000

    first loop in coins

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,15 +4,15 @@
 
 def makeChange(coins, total):
     """Function to return minimum coin number for change"""
-    if total <= 0:
+    if total == 0:
         return 0
 
     dp = [total + 1] * (total + 1)
     dp[0] = 0
-
-    for i in range(1, total + 1):
-        for j in coins:
-            if i - j >= 0:
-                dp[i] = min(dp[i], dp[i - j] + 1)
+    
+    for j in coins:
+        for i in range(j, total + 1):
+            #if i - j >= 0:
+            dp[i] = min(dp[i], dp[i - j] + 1)
 
     return dp[total] if dp[total] != total + 1 else -1
