894831f1f4294abf22b6c9575a88595e4efa27ec
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 20:22:39 +0000

    sort for optimization

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,8 +6,9 @@ def makeChange(coins, total):
     """Function to return minimum coin number for change"""
     if total <= 0:
         return 0
+    coins.sort(reverse=True)
 
-    dp = [float('inf')] * (total + 1)
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
 
     for i in range(1, total + 1):
@@ -15,4 +16,4 @@ def makeChange(coins, total):
             if (i - j >= 0):
                 dp[i] = min(dp[i], 1 + dp[i - j])
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != total + 1 else -1
