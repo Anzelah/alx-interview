5c385074af3ac7c21d998ccecc9401a12956ff97
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 20:45:21 +0000

    optimization

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -10,7 +10,6 @@ def makeChange(coins, total):
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
-    coins.sort()
     for i in range(1, total + 1):
         for j in coins:
             if i - j >= 0:
