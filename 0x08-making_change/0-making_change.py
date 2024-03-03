97fb6b8a16c3d1efea6b89bf2e3c6ab1480dfaae
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 21:51:03 +0000

    l

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,13 +7,13 @@ def makeChange(coins, total):
     if total <= 0:
         return 0
 
-    dp = [float('inf')] * (total + 1)
+    dp = [total + 1] * (total + 1)
     dp[0] = 0
 
     for j in coins:
         if j > total:
             break
         for i in range(j, total + 1):
-            dp[i] = min(dp[i], dp[i - j] + 1)
+            dp[i] = min(dp[i], 1 + dp[i - j])
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return dp[total] if dp[total] != total + 1 else -1
