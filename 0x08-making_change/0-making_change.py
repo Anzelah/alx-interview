d7cb9462d480df2526864aea677988fdd2441dd1
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 20:56:32 +0000

    optimization

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -9,10 +9,10 @@ def makeChange(coins, total):
 
     dp = [total + 1] * (total + 1)
     dp[0] = 0
-    
+
     for j in coins:
         for i in range(j, total + 1):
-            #if i - j >= 0:
-            dp[i] = min(dp[i], dp[i - j] + 1)
+            if i - j >= 0:
+                dp[i] = min(dp[i], dp[i - j] + 1)
 
     return dp[total] if dp[total] != total + 1 else -1
