4da8118bfd3e3d5bec4a383a7319e31193b87ede
Anzelah <jepkoechanzellah@gmail.com>
2024-03-03 20:10:13 +0000

    check that sum is less or equal to coin first

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,12 +6,14 @@ def makeChange(coins, total):
     """Function to return minimum coin number for change"""
     if total <= 0:
         return 0
+    coins.sort()
 
     dp = [total + 1] * (total + 1)
     dp[0] = 0
 
     for i in range(1, total + 1):
         for j in coins:
-            dp[i] = min(dp[i], 1 + dp[i - j])
+            if (i - j >= 0):
+                dp[i] = min(dp[i], 1 + dp[i - j])
 
     return dp[total] if dp[total] != total + 1 else -1
