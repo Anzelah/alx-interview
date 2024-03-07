e95d0228a540b1071657da283d089def60624214
root <Anzelah>
2024-03-07 09:36:24 -0800

    use the shared edges method minus land

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,24 +4,20 @@
 
 def island_perimeter(grid):
     """Function to calculate island perimeter"""
-    
+
     length = len(grid[0])
     width = len(grid)
 
-    per = 0
-    e = 0
+    land_cell = 0
+    shared_edge = 0
 
     for r in range(width):
         for c in range(length):
             if (grid[r][c] == 1):
-                per += 1
+                land_cell += 1
                 if (c > 0 and grid[r][c - 1] == 1):
-                    e += 1
+                    shared_edge += 1
                 if (r > 0 and grid[r - 1][c] == 1):
-                    e += 1
-                #if (grid[r - 1][c] == 0):
-                #    per += 1
-                #if (grid[r + 1][c] == 0):
-                 #   per += 1
+                    shared_edge += 1
 
-    return per * 4 - e * 2
+    return (land_cell * 4) - (shared_edge * 2)
