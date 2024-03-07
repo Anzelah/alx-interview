0a6f3284a2c0ad0a53acfe03717077b546995e7a
root <Anzelah>
2024-03-07 09:00:47 -0800

    grid is completely sorrounded by water

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -8,13 +8,13 @@ def island_perimeter(grid):
     for r in range(len(grid)):
         for c in range(len(grid[0])):
             if (grid[r][c] == 1):
-                if (c == 0 or grid[r][c - 1] == 0):
+                if (grid[r][c - 1] == 0):
                     per += 1
-                if (c == len(grid[0]) - 1 or grid[r][c + 1] == 0):
+                if (grid[r][c + 1] == 0):
                     per += 1
-                if (r == 0 or grid[r - 1][c] == 0):
+                if (grid[r - 1][c] == 0):
                     per += 1
-                if (r == len(grid[r]) - 1 or grid[r + 1][c] == 0):
+                if (grid[r + 1][c] == 0):
                     per += 1
 
     return per
