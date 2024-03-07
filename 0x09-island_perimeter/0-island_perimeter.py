706e568dd7358770926a259c466cfd70d0c8a544
root <Anzelah>
2024-03-07 09:18:47 -0800

    trying another method o see

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -4,21 +4,24 @@
 
 def island_perimeter(grid):
     """Function to calculate island perimeter"""
+    
     length = len(grid[0])
     width = len(grid)
 
     per = 0
+    e = 0
 
     for r in range(width):
         for c in range(length):
             if (grid[r][c] == 1):
-                if (grid[r][c - 1] == 0):
-                    per += 1
-                if (grid[r][c + 1] == 0):
-                    per += 1
-                if (grid[r - 1][c] == 0):
-                    per += 1
-                if (grid[r + 1][c] == 0):
-                    per += 1
+                per += 1
+                if (c > 0 and grid[r][c - 1] == 1):
+                    e += 1
+                if (r > 0 and grid[r - 1][c] == 1):
+                    e += 1
+                #if (grid[r - 1][c] == 0):
+                #    per += 1
+                #if (grid[r + 1][c] == 0):
+                 #   per += 1
 
-    return per
+    return per * 4 - e * 2
