e90f2857e748c236fc6776e2c53eb2a943b172b4
root <Anzelah>
2024-03-07 08:56:33 -0800

    calculate island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Island perimeter"""
+
+
+def island_perimeter(grid):
+    per = 0
+    for r in range(len(grid)):
+        for c in range(len(grid[0])):
+            if (grid[r][c] == 1):
+                if (c == 0 or grid[r][c - 1] == 0):
+                    per += 1
+                if (c == len(grid[0]) - 1 or grid[r][c + 1] == 0):
+                    per += 1
+                if (r == 0 or grid[r - 1][c] == 0):
+                    per += 1
+                if (r == len(grid[r]) - 1 or grid[r + 1][c] == 0):
+                    per += 1
+
+    return per
