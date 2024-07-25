7fd362eff9f4e16ce48ebb2674531fd75b5db92d
Anzelah <jepkoechanzellah@gmail.com>
2024-07-25 14:32:56 +0000

    just concepts no code

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Solving pascals triangle
+"""
+
+
+def pascal_triangle(n):
+    """
+    Return list of lists of integers representing the Pascal’s triangle of n.
+    Returns: an empty list if n <= 0
+    You can assume n will be always an integer
+    """
+    if n <= 0:
+        return []
+    row_0 =       [1]
+    row_1 =      [1, 1]
+    row_3 =     [1, 2, 1]
+    row_4 =    [1, 3, 3, 1]
+    row_5 =   [1, 4, 6, 4, 1]
+    row_6 = [1, 5, 10, 10, 5, 1]
+    elements = nth row + 1
+    rows start at 0
+    all numbers at both ends wll always be 1
