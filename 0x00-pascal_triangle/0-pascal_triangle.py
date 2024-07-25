00abb3edaa95afebcc4135dbbce48f3845c43784
Anzelah <jepkoechanzellah@gmail.com>
2024-07-25 14:51:26 +0000

    just concepts no code

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -11,12 +11,14 @@ def pascal_triangle(n):
     """
     if n <= 0:
         return []
-    row_0 =       [1]
-    row_1 =      [1, 1]
-    row_3 =     [1, 2, 1]
-    row_4 =    [1, 3, 3, 1]
-    row_5 =   [1, 4, 6, 4, 1]
-    row_6 = [1, 5, 10, 10, 5, 1]
-    elements = nth row + 1
-    rows start at 0
-    all numbers at both ends wll always be 1
+    #row_0 =       [1]
+    #row_1 =      [1, 1]
+    #row_3 =     [1, 2, 1]
+    #row_4 =    [1, 3, 3, 1]
+    #row_5 =   [1, 4, 6, 4, 1]
+    #elements = nth row + 1
+    #rows start at 0
+    #all numbers at both ends wll always be 1
+    #n is number of rows to be printed
+    for i in range(1, n+1): # columns of each row
+        print(i)
